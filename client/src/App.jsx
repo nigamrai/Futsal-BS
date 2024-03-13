843318144b2ad21099629e6378abfba779b90474
RaiNigam <nigamrai2020@gmail.com>
2024-03-13 16:51:32 +0545

    login frontend added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
-
 function App() {
-
   return (
     <Routes>
-      <Route path="/signup" element={<Signup/>}>
-
-      </Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
