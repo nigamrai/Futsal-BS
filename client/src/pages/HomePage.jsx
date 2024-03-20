4f62b63725553c1722f88162a021ce8e8828fc2f
RaiNigam <nigamrai2020@gmail.com>
2024-03-20 17:15:04 +0545

    homepage added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,20 +4,21 @@ import HomeLayout from "../Layouts/HomeLayout";
 import background from '../assets/images/background.png';
 import { futsalDetails } from "../redux/slices/futsalSlice";
 function HomePage(){
-    const futsalData=useSelector((state)=>state?.futsal?.futsalData);
+    const {futsalData}=useSelector((state)=>state?.futsal);
     const dispatch=useDispatch();
     async function getFutsalDetails(){
-        await dispatch(futsalDetails);
+        await dispatch(futsalDetails());
     }
     useEffect(()=>{
         getFutsalDetails();
     },[])
+    
     return <HomeLayout>
         <div style={{
             backgroundImage:`url(${background})`,
             backgroundSize:'cover'
         }} className="h-[400px] px-[100px] flex flex-col justify-center gap-2">
-            <h1 className="text-6xl text-[#FFDC58] font-bold w-[700px]">{futsalData.futsalName}</h1>
+            <h1 className="text-6xl text-[#FFDC58] font-bold w-[700px]">Book {futsalData.map((futsal)=>futsal.futsalName)} </h1>
             <p className="text-white font-bold text-3xl">Booking made easier</p>
         </div>
     </HomeLayout>
